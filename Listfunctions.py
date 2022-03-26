b56e739fdcbce27e0336efaa48c89144b23e85f9
Jai Ailawadi <ailawadij@gmail.com>
2022-03-25 21:04:21 -0400

    Listfunctions

---
+++ Listfunctions.py
@@ -0,0 +1,8 @@
+lucky_number = [4, 5, 19, 10, 26, 25]
+friends = ["Sam", "Shaan", "Jackson", "Shaan", "Shaan","Shaan"]
+lucky_number.reverse()
+print(lucky_number)
+friends2 = friends.copy()
+print(friends2)
+friends3 = friends2.sort()
+print(friends3)
---
+++ Listsintro.py
@@ -0,0 +1,3 @@
+friends = ["Shaan", "Sam", "Jackson", "Matt", "Abhi", "Vinny"]
+friends[1] = "Jai"
+print(friends[1:4])
