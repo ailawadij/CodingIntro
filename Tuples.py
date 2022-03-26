5d2a5d3b3bc2250618911cd9d1aed78aaf4b9429
Jai Ailawadi <ailawadij@gmail.com>
2022-03-26 01:42:36 -0400

    Notes

--- Tuples.py
+++ Tuples.py
@@ -1,3 +1,3 @@
 coordinates = [(4, 5), (6, 8), (9, 29)]
 print(coordinates[2])
-Tuples Cannot be changed, lists can be changed in subsequent functions
+# Tuples Cannot be changed, lists can be changed in subsequent functions
